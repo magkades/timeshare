9eb2abeeb7e84d813bef1b6f3cb81e7d66bc791c
yannakyp <yannakyp@gmail.com>
2015-03-07 15:41:05 +0000

    fixed route

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/api/requests', require('./api/request'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
