b0021d6be62779aa858cbd2c047f5f08a8005392
yannakyp <yannakyp@gmail.com>
2015-03-07 17:26:04 +0000

    added requests

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,10 +5,115 @@ angular.module('timeshareApp')
     $scope.requests = [];
     $scope.user = Auth.getCurrentUser();
 
-    $http.get('/api/requests').success(function(requests) {
-      $scope.requests = requests;
+
+    $scope.requests = [
+            {
+                '_id': 1,
+                'description': 'request1',
+                'requester':
+                {
+                    '_id': '1',
+                    'name': 'user1'
+                },
+                'bidder':
+                {
+                    '_id': '2',
+                    'name': 'user2'
+                },
+                'credit': 10,
+                'category': 'category1',
+                'status': 'open'
+     },
+            {
+                '_id': 2,
+                'description': 'request2',
+                'requester':
+                {
+                    '_id': '3',
+                    'name': 'user3'
+                },
+                'bidder':
+                {
+                    '_id': '2',
+                    'name': 'user2'
+                },
+                'credit': 20,
+                'category': 'category1',
+                'status': 'open'
+      },
+            {
+                '_id': 3,
+                'description': 'request3',
+                'requester':
+                {
+                    '_id': '1',
+                    'name': 'user1'
+                },
+                'bidder':
+                {
+                    '_id': '54fb0748fe56bf75396bd07e',
+                    'name': 'mcarmen'
+                },
+                'credit': 10,
+                'category': 'category2',
+                'status': 'open'
+            },
+            {
+                '_id': 4,
+                'description': 'request4',
+                'requester':
+                {
+                    '_id': '54fb0748fe56bf75396bd07e',
+                    'name': 'mcarmen'
+                },
+                'bidder':
+                {
+                    '_id': '5',
+                    'name': 'user5'
+                },
+                'credit': 10,
+                'category': 'category2',
+                'status': 'open'
+            },
+            {
+                '_id': 4,
+                'description': 'request4',
+                'requester':
+                {
+                    '_id': '54fb0748fe56bf75396bd07e',
+                    'name': 'mcarmen'
+                },
+                'bidder':
+                {
+                    '_id': '5',
+                    'name': 'user5'
+                },
+                'credit': 10,
+                'category': 'category2',
+                'status': 'under_offer'
+            },
+        {
+            '_id': 5,
+            'description': 'request5',
+            'requester':
+            {
+                '_id': '54fb0748fe56bf75396bd07e',
+                'name': 'mcarmen'
+            },
+            'bidder':
+            {
+                '_id': '5',
+                'name': 'user5'
+            },
+            'credit': 10,
+            'category': 'category2',
+            'status': 'under_offer'
+        }
+
+
+        ];
+
       updateLists();
-    });
 
     $scope.newRequest = function() {
       if($scope.newRequest === '') {
@@ -34,15 +139,16 @@ angular.module('timeshareApp')
         if (status === 'rejected') {
           request.bidder = {};
         }
-        //$http.put('/api/requests/'+request._id, request).success(function() {
             updateLists();
-     //   });
     }
 
     function updateLists () {
+      //  console.log($scope.user);
         $scope.myRequests = $scope.requests.filter(function(request){
             return request.requester._id === $scope.user._id;
         });
+
+        console.log( $scope.myRequests);
         $scope.myOffers = $scope.requests.filter(function(request){
             return request.bidder._id === $scope.user._id;
         });
